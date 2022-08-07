edaa0102bbe923850b333268ffa6cff8448da79e
Aditya Rizqi <adityarizqi1010@gmail.com>
2022-08-07 19:40:52 +0700

    Logout bug fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,7 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, Tokenable;
 
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    public $guarded = [];
 
     protected $hidden = [
         'password',
